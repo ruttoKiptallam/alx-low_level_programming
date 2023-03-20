c4be69fd3b10e1c6a4cfb0e9b8c5a3bb72156762
ruttokiptallam <rutttokiptallam@gmail.com>
2023-03-20 13:05:50 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,16 @@
-#!/bin/bash
-{	puts("\"Programming is like building a multilingual puzzle");
-	return (0); }
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This function prints a message to the console using the
+ *              puts() function.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
