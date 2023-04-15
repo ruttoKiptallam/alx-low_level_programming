5c84d1d17a8a60cba2c65d027afe512c3ded7364
ruttokiptallam <rutttokiptallam@gmail.com>
2023-04-14 17:54:33 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,46 +9,30 @@
  *num1 and num2 should only be composed of digits
  *If not, print Error, followed by a new line, and exit with a status of 98
  */
-int main (void)
+void _is_zero(char *argv[])
 {
-	int num1;
-	int num2;
-	int mul;
-	int toBase = 10;
+	int i, isn1 = 1, isn2 = 1;
 
-	mul = toBase; 10
-		   ;
-	mul = num1 + num2;
-	while(mul = toBase) 10;
-	 {
-		 printf("%d\n", mul);
-	 }
-        do
+	for (i = 0; argv[1][i]; i++)
+		if (argv[1][i] != '0')
+		{
+			isn1 = 0;
+			break;
+		}
+
+	for (i = 0; argv[2][i]; i++)
+		if (argv[2][i] != '0')
+		{
+			isn2 = 0;
+			break;
+		}
+
+	if (isn1 == 1 || isn2 == 1)
 	{
-		printf("error\n");	
-	
+		printf("0\n");
+		exit(0);
 	}
-        while (scanf("%d%d", &num1, &num2));
-        
-                 exit (98);
-	do
-        {
-		printf("error\n");
-	}
-         
-
-}	//end of loop
-
-#include <math.h>
-#include <stdlib.h>
-/**
- * _checknum - determines length of the number
- * and checks if number is in base 10.
- * @argv: arguments vector.
- * @n: row of the array.
- *
- * Return: length of the number.
- */
+}
 int _checknum(char *argv[], int n)
 {
 	int ln;
@@ -61,4 +45,46 @@ int _checknum(char *argv[], int n)
 		}
 
 	return (ln);
+}int main(int argc, char *argv[])
+{
+	int ln1, ln2, lnout, add, addl, i, j, k, ca;
+	char *nout;
+
+	if (argc != 3)
+		printf("Error\n"), exit(98);
+	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
+	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
+	if (nout == NULL)
+		printf("Error\n"), exit(98);
+	nout = _initialize_array(nout, lnout);
+	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+	for (; k >= 0; k--, i--)
+	{
+		if (i < 0)
+		{
+			if (addl > 0)
+			{
+				add = (nout[k] - '0') + addl;
+				if (add > 9)
+					nout[k - 1] = (add / 10) + '0';
+				nout[k] = (add % 10) + '0';
+			}
+			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
+		}
+		if (j < 0)
+		{
+			if (nout[0] != '0')
+				break;
+			lnout--;
+			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
+			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+		}
+		if (j >= 0)
+		{
+			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
+			addl = add / 10, nout[k] = (add % 10) + '0';
+		}
+	}
+	printf("%s\n", nout);
+	return (0);
 }
