a87c1d886d69322fc8eb1723fea6296ca7767cd5
elias rutto <rutttokiptallam@gmail.com>
2023-04-17 07:46:44 -0700

    0x0D-preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*printing the  name of the file it was compiled from, followed by a new line
+ *
+ *
+ *
+ */
+
+int main ()
+{
+	printf("%s\n", _FILE_);
+	return O;	
+}	
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef FUNTIONS_LIKE_MACRO_H
+#define FUNTIONS_LIKE_MACRO_H
+
+/*
+ *writting a function-like macro ABS(x) that computes the absolute value of a number x
+ *
+ *
+ */
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+#endif
