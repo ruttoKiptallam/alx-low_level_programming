7c2632301c59b6b6f2a284ce5614d0e691d6e3ac
elias rutto <rutttokiptallam@gmail.com>
2023-04-17 04:24:55 -0700

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    struct dog; 
+
+    char name []= "Poppy";
+    float age = 3.5;
+     char owner [] = "Bob";
+    
+    printf("My name is %s, and I am %.1f :) - Woof!\n", name, age);
+    return (0);
+}
