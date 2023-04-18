d641ba47558ad2004bdfac8a6db1d1a65e49e0da
elias rutto <rutttokiptallam@gmail.com>
2023-04-18 09:24:30 -0700

    0x0E-structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,15 +9,15 @@
 
 struct dog
 {
-   char *name;
-   float age;
-   char *owner;    
+     char *name;
+     float age;
+     char *owner;    
 };
 /*dog_t -typedef for struct dog*/
 typedef struct dog dog_t;
 
- void init_dog(struct dog *d, char *name, float age, char *owner);
- void print_dog(struct dog *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
  dog_t *new_dog(char *name, float age, char *owner);
  void free_dog(dog_t *d);
 
