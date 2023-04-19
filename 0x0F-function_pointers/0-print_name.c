acbc779f4d8a5ce07a2221b94bf7b1617833dee5
elias rutto <rutttokiptallam@gmail.com>
2023-04-19 06:43:27 -0700

    0x0F-function_pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_name_as_is - prints a name as is
+ * @name: name of the person
+ * @f: a funtion pointerthat takes character pointer
+ * Return: Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if(name == NULL || f == NULL)
+	{
+		return;
+	}
+	else
+	{
+		printf("%s\n", name);
+	}
+	f(name);
+}
