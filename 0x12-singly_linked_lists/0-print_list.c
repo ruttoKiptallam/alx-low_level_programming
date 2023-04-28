9c2fbbfd3c1e2b0c56cab6390eb0d5ea9b669efb
elias rutto <rutttokiptallam@gmail.com>
2023-04-28 12:11:07 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,40 +3,38 @@
 #include <string.h>
 #include "lists.h"
 /**
+*print_list: print list of strings 
 *Prototype: size_t print_list(const list_t *h);
-*
-*h: string
+*size_t: space allocated to data
+*@h: string
 *If string is NULL, print [0] (nil)
-*
+*@r: number of nodes
 *Return: the number of nodes
 */
-
 size_t print_list(const list_t *h)
 {
-    size_t r = 0;
-    
+	size_t r = 0;
 
-    if(h != NULL)
-    {
-        printf("[0] (nil\n)");
-    }
-   else
-   {
-            printf("[%d] %s", h->len, h->str);
-   }
-            r++;
-            h = h->next;
-
-            return (r);
+	if (h != NULL)
+	{
+		printf("[0] (nil\n)");
+	}
+	else
+	{
+		printf("[%d] %s", h->len, h->str);
+	}
+	r++;
+	h = h->next;
+	return (r);
 }
 int main()
-{ 
-        const list_t *h;
-        while (h != NULL)
-    {
-        printf("%s\n", h->str);
-        h = h->next;
-    }
-        return(0);
-}
+{
+	const list_t *h;
 
+	while (h != NULL)
+	{
+		printf("%s\n", h->str);
+		h = h->next;
+	}
+	return (0);
+}
