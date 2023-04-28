be4f7befb904230ecfd647eff7d8bfc9f1505fb0
elias rutto <rutttokiptallam@gmail.com>
2023-04-28 10:49:12 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -29,3 +29,14 @@ size_t print_list(const list_t *h)
 
             return (r);
 }
+int main()
+{ 
+        const list_t *h;
+        while (h != NULL)
+    {
+        printf("%s\n", h->str);
+        h = h->next;
+    }
+        return(0);
+}
+
